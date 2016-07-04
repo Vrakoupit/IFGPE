61b272070d8f8be0bdaeec41f1adcd95532a9594
Vincent Curt <vincent.curt@etu-webschoolfactory.fr>
2016-07-04 11:20:19 +0200

    Css & fix html
    Ajout de style aux promotions, centres et newsletter. Texte centré

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 </section>
 <div class="container">
 	<section id="courses">
+	<h1>Nos formations</h1>
 		<div>
-			<h1 class="col-sm-12">IT &amp; Service Management</h1>
+			<h2 class="col-sm-12">IT &amp; Service Management</h2>
 			<ul id="itservice">
 				<li class="itemCourse"><a href=""><div class="courses">ITIL</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">Business Analysis</div></a></li>
@@ -16,7 +17,7 @@
 				<li class="itemCourse"><a href=""><div class="courses">Cisco</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">VMWare</div></a></li>
 			</ul>
-			<h1 class="col-sm-12">Gouvernances des SI</h1>
+			<h2 class="col-sm-12">Gouvernances des SI</h2>
 			<ul id="gouvernance">
 				<li class="itemCourse"><a href=""><div class="courses">COBIT 5</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">CMMI</div></a></li>
@@ -24,26 +25,23 @@
 				<li class="itemCourse"><a href=""><div class="courses">TOGAF</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">Citrix</div></a></li>
 			</ul>
-			<h1 class="col-sm-12">Gestion de projet</h1>
+			<h2 class="col-sm-12">Gestion de projet</h2>
 			<ul id="gdp">
 				<li class="itemCourse"><a href=""><div class="courses">Prince 2</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">Agile</div></a></li>
 				<li class="itemCourse"><a href=""><div class="courses">PMI - PMP</div></a></li>
+				<li class="itemCourse" id="microsoftCourse"><a href=""><div class="courses">Microsoft Windows Server</div></a></li>
+				<li class="itemCourse" id="languageCourse"><a href=""><div class="courses">Formations langues</div></a></li>
 			</ul>
-			<h1 class="col-sm-12">Cours Microsoft</h1>
-			<ul id="microsoft">
-				<li class="itemCourse"><a href=""><div class="courses">Microsoft Windows Server</div></a></li>
-			</ul>
-			<h3>Formations en langues</h3>
 		</div>
 	</section>
 	<section id="promotions">
 		<div>
 			<h1>Nos promotions</h1>
 			<ul id="promotion">
-				<li class="col-sm-4"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
-				<li class="col-sm-4"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
-				<li class="col-sm-4"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
+				<li class="col-sm-4"><div class="promotions"><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
+				<li class="col-sm-4"><div class="promotions"><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
+				<li class="col-sm-4"><div class="promotions"><img src="https://unsplash.it/150/150?image=0" alt=""><p>Formations Reference</p></div></li>
 			</ul>
 		</div>
 	</section>
@@ -52,36 +50,36 @@
 			<h1>Actualités</h1>
 			<div class="col-sm-4">
 				<h2>Le Lorem Ipsum</h2>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
+				<p class="actuParagraphe">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
 			</div>
 			<div class="col-sm-4">
 				<h2>Le Lorem Ipsum</h2>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
+				<p class="actuParagraphe">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
 			</div>
 			<div class="col-sm-4">
 				<h2>Le Lorem Ipsum</h2>
-				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
+				<p class="actuParagraphe">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique.</p>
 			</div>
 		</div>
 	</section>
 	<section id="calendar">
-		<h3>Voir les prochaines sessions de formation</h3>
+		<h4>Voir les prochaines sessions de formation</h4>
 	</section>
 	<section id="partners" class="col-sm-12">
 		<div>
 			<h1>Nos centres partenaires</h1>
 			<ul id="centrePartenaire">
-				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>Avnet Academy</p></div></li>
-				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>VMWare Partner</p></div></li>
-				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>ETS</p></div></li>
-				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt=""><p>IBM</p></div></li>
+				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt="" class="imgCentrePartenaire"><p>Avnet Academy</p></div></li>
+				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt="" class="imgCentrePartenaire"><p>VMWare Partner</p></div></li>
+				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt="" class="imgCentrePartenaire"><p>ETS</p></div></li>
+				<li class="col-sm-3"><div><img src="https://unsplash.it/150/150?image=0" alt="" class="imgCentrePartenaire"><p>IBM</p></div></li>
 			</ul>
 		</div>
 	</section>
 	<section id="newsletterFooter">
 		<div class="container">
 			<p>Inscrivez vous à la newsletter</p>
-			<label for="newsletter">E-mail<input type="mail" id="newsletter"></label>
+			<label for="newsletter" id="labelNewsletter">E-mail<input type="mail" id="newsletter"></label>
 		</div>
 	</section>
 </div>
